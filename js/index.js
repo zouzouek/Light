6d45bc260271ecb13f9f655e460d31d962b610c5
joseph <joseph@joseph-Satellite-Pro-C40-A>
2014-09-05 10:07:38 +0300

    Final Product

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var signingList=[];
 
 $(document).ready(function() {
     $(".message").hide();
@@ -10,8 +11,18 @@ $(document).ready(function() {
         colorful = !colorful;
         playanimation();
     });
-    $("#join").click(function(){
-        openShare(); 
+    $("#join").click(function() {
+        openSign();
+    });
+    $("#sign").click(function() {
+        if (ValidateInput()) {
+            writeSignature();
+
+            emptyError();
+        } else {
+            showError();
+        }
+
     });
 
 });
@@ -37,7 +48,66 @@ function playanimation() {
     }
 }
 
-function openShare(){
-    $("#message").fadeOut("fast");
+function openShare() {
+    $("#petition").fadeOut("fast");
     $("#share").fadeIn("slow");
+}
+function openSign() {
+    $("#message").fadeOut("fast");
+    $("#petition").fadeIn("slow");
+}
+
+function ValidateInput() {
+
+    if (!$("#name").val() || !$("#age").val()) {
+        return false;
+    }
+    return true;
+}
+function showError() {
+    $("#error").html("Please Fill all the fields before signing.")
+}
+
+function emptyError() {
+    $("#error").html("");
+}
+
+function writeSignature() {
+    var name = $("#name").val();
+    var age = $("#age").val();
+
+    var myFirebaseRef = new Firebase("https://torid-fire-4034.firebaseio.com/");
+    myFirebaseRef.push({
+        name: name,
+        age: age
+    });
+    myFirebaseRef.once('value', function(snapshot) {
+        
+        fillList(snapshot.val());
+
+    }, function(errorObject) {
+        console.log('The read failed: ' + errorObject.code);
+    });
+    
+    openShare();
+}
+
+function fillList(list){
+    signingList=[];
+    for( var item in list){
+        var person = list[item];
+        var signature =person.name+" "+person.age;
+        signingList.push(signature);
+    }
+    setSignatureNb();
+}
+
+function setSignatureNb(){
+      var signatureNb=1;
+     signatureNb= signingList.length;
+    $("#nb-singatures").html(signatureNb);
+}
+
+function displayList(){
+    
 }
